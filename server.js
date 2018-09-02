d252a6fed58ab74e92c44cd9115bfc0f41d7a906
Abdullah khan <khanabdullah4289951@gmail.com>
2018-09-03 01:22:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/getValidityofQrcode', function (req, res) {
         connection.query(query, function (err, result) {
             //connection.release();
             console.log(result);
-            res.json(result);
+            res.json(result[0]);
         });
 
         connection.on('error', function (err) {

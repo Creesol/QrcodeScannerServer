fd7811ca975575b35b9a80e6817c02a990581f6f
kashif zahid <kashif.zahid18@gmail.com>
2018-09-04 02:35:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/getUserByDate', function (req, res) {
     });
 });
 app.get('/getTotalqr',function(req,res){
-    var query="select count(product_qr_code_id) AS Total from QRcodeScanner.product_info";
+    var query="select count(product_qr_code) AS Total from QRcodeScanner.product_info";
     con.getConnection(function(err,connection){
         connection.query(query, function (err, result) {
             res.send(result);
